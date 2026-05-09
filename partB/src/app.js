fda988df44a08fbcbeb27998647f7eb3724af83f
hunaa2006 <hunaahun0@gmail.com>
2026-05-09 22:47:55 +0800

    feat: setup sqlite database and express server

---
+++ partB/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./database');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/health', (req, res) => {
+  res.json({ success: true, message: 'Server is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
+
+module.exports = app;
